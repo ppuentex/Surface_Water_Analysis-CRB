68c80954abfdfec693593d107eae7f4bad0b67e5
Patricia <ppuentex@gmail.com>
2025-02-21 13:30:14 -0700

    added code to calculate the transitions in urban areas

--- Codes/utils/helper_functions.py
+++ Codes/utils/helper_functions.py
@@ -128,3 +128,73 @@ class WaterTransitionAnalyzer:
             print(f"Processed HUC {huc_id}")
 
         return pd.DataFrame(trans_results)
+    
+    def calculate_transition_urban(self, mode_path1, mode_path2, urban_path, huc_raster):
+        '''Analyzes water transitions in urban and non-urban areas within each huc4 subbasin'''
+        water_period1,_ = self.load_raster(mode_path1)
+        water_period2,_ = self.load_raster(mode_path2)
+        land_class,_ = self.load_raster(urban_path)
+
+        #define water pixel labels 
+        nowater_label = 1 
+        seasonal_label = 2
+        permanent_label = 3 
+
+        # Create urban masks
+        stable_urban_mask = (land_class == 2)
+        gain_urban_mask = (land_class == 1)
+
+        # Create transition masks
+        trans_masks = {
+            "perm_nowater": (water_period1 == permanent_label) & (water_period2 == nowater_label),
+            "perm_seasonal": (water_period1 == permanent_label) & (water_period2 == seasonal_label),
+            "seasonal_nowater": (water_period1 == seasonal_label) & (water_period2 == nowater_label),
+            "seasonal_perm": (water_period1 == seasonal_label) & (water_period2 == permanent_label),
+            "nowater_seasonal": (water_period1 == nowater_label) & (water_period2 == seasonal_label),
+            "nowater_perm": (water_period1 == nowater_label) & (water_period2 == permanent_label),
+        }
+
+        # Get unique HUC labels
+        unique_labels = np.unique(huc_raster)
+        unique_labels = unique_labels[unique_labels != 0]  # Exclude background (0)
+        trans_results = []
+        for huc_id in unique_labels:
+            huc_mask = (huc_raster == huc_id)
+            huc_count = np.sum(huc_mask)
+
+            gained_urban = np.sum(huc_mask & gain_urban_mask)
+            stable_urban = np.sum(huc_mask & stable_urban_mask)
+
+            percent_urbanization = ((gained_urban + stable_urban) / huc_count) * 100
+
+            # Calculate transition areas
+            dry_area = sum(np.sum(huc_mask & trans_masks[key]) for key in ["perm_nowater", "perm_seasonal", "seasonal_nowater"])
+            wet_area = sum(np.sum(huc_mask & trans_masks[key]) for key in ["seasonal_perm", "nowater_seasonal", "nowater_perm"])
+
+            total_dry_transition_area = (dry_area * self.pixel_area) / self.km_scale
+            total_wet_transition_area = (wet_area * self.pixel_area) / self.km_scale
+
+            #urban specific water transitions 
+            stable_dry_area = sum(np.sum(huc_mask & stable_urban_mask & trans_masks[key]) for key in ["perm_seasonal", "perm_nowater", "seasonal_nowater"])
+            gained_dry_area = sum(np.sum(huc_mask & gain_urban_mask & trans_masks[key]) for key in ["perm_seasonal", "perm_nowater", "seasonal_nowater"])
+
+            stable_wet_area = sum(np.sum(huc_mask & stable_urban_mask & trans_masks[key]) for key in ["seasonal_perm", "nowater_perm", "nowater_seasonal"])
+            gained_wet_area = sum(np.sum(huc_mask & gain_urban_mask & trans_masks[key]) for key in ["seasonal_perm", "nowater_perm", "nowater_seasonal"])
+
+            # Store results
+            trans_results.append({
+                'huc4': huc_id,
+                'HUC_count': huc_count,
+                'gained_urban_area': (gained_urban * self.pixel_area) / self.km_scale,
+                'stable_urban_area': (stable_urban * self.pixel_area) / self.km_scale,
+                'dry_transition_area': total_dry_transition_area,
+                'wet_transition_area': total_wet_transition_area,
+                'stable_urban_dry_area': (stable_dry_area * self.pixel_area) / self.km_scale,
+                'gained_urban_dry_area': (gained_dry_area * self.pixel_area) / self.km_scale,
+                'stable_urban_wet_area': (stable_wet_area * self.pixel_area) / self.km_scale,
+                'gained_urban_wet_area': (gained_wet_area * self.pixel_area) / self.km_scale,
+                'urbanization_percent': percent_urbanization
+            })
+            print(f"Processed HUC {huc_id}")
+        
+        return pd.DataFrame(trans_results)
